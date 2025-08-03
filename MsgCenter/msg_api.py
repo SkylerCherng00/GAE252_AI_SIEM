88834975b259d6f59d472a33747a16fce31c8770
SkylerCherng00 <abevvexe@gmail.com>
2025-08-04 07:46:02 +0800

    08042025

--- MsgCenter/msg_api.py
+++ MsgCenter/msg_api.py
@@ -288,4 +288,4 @@ async def update_config_factory(request: AllConfigUpdateRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
